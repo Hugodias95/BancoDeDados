b99a43a6533039667161f21cfd1d7be69e740754
Hugodias95 <hugo.dias222@gmail.com>
2023-03-10 19:28:26 -0300

    Initial commit

---
+++ ProjetoConsultor/src/tools/ConexaoBD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tools;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author aluno
+ */
+public class ConexaoBD {
+    
+}
